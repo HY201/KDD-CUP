99d227c72af7ce3a742ff30ab76760598f9ecdb1
admin <admin@Czzzzzzzz.local>
2016-05-19 20:47:59 +0800

    add auc criterion

--- FinalHomework.py
+++ FinalHomework.py
@@ -6,6 +6,7 @@ Created on Thu May 19 08:53:47 2016
 """
 from sklearn import tree
 from sklearn import neighbors
+from sklearn.metrics import roc_curve, auc
 import pandas as pd
 
 #data is divided into two parts : traning data and test data
@@ -31,12 +32,16 @@ def classify(traning_data, training_label, test_data):
     test_label = clf.predict(test_data)
     return test_label
 
-def classify2(traning_data, training_label, test_data):
+def classify2(traning_data, training_label, test_data, testing_label):
     clf = neighbors.KNeighborsClassifier(15)
     clf = clf.fit(traning_data, training_label)
 
-    test_label = clf.predict(test_data)
-    return test_label
+#    test_label = clf.predict(test_data)
+    probas_  = clf.predict_proba(test_data)
+    fpr, tpr, thresholds = roc_curve(testing_label, probas_[:, 0])
+    roc_auc = auc(fpr, tpr)    
+    
+    return roc_auc
     
 def replaceMissingValue(data):
     data  = data.fillna(0)
@@ -60,7 +65,7 @@ def main():
     
     part_training_data = training_data.iloc[:, 5:7]
     part_testing_data = testing_data.iloc[:, 5:7]
-    print classify2(part_training_data, appe_label, part_testing_data)    
+    print classify2(part_training_data, appe_label, part_testing_data, appe_label_test)    
     
     
 main()
