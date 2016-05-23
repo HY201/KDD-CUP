847db7065e0698d4f8aaec8d1b59423e67cb45b6
admin <admin@101.76.83.110.broad.xm.fj.dynamic.163data.com.cn>
2016-05-23 15:04:16 +0800

    update

--- FinalHomework.py
+++ FinalHomework.py
@@ -16,12 +16,18 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn import preprocessing
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectFromModel, SelectPercentile
 from sklearn.metrics import (precision_score, recall_score,f1_score, precision_recall_fscore_support)
 from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 
 import pandas as pd
 import MutiLabelEncoder as labelencoder
+import split as sp
+import numpy as np
 import random
 
 #data is divided into two parts : traning data and test data
@@ -31,7 +37,8 @@ def splitData(dataSet, num_of_testing):
     return training_data, testing_data
 
 def loadData(filePath):
-    df = pd.read_csv(filePath, sep='\t')
+#    df = pd.read_csv(filePath, sep='\t')
+    df = sp.getTwoTypesData(filePath)
     training_data, testing_data = splitData(df, 1000)
     return training_data, testing_data
     
@@ -40,51 +47,58 @@ def loadLabel(filePath):
     training_label, testing_label = splitData(df, 1000)
     return training_label[0], testing_label[0]
 
-#testing_label and test_data isn't used temperorily
-def classify(traning_data, training_label, test_data, testing_label):
-    fold = 2
+def estimate(classfier, traning_data, training_label, test_data, testing_label):
+    fold = 5
     skf = StratifiedKFold(training_label, fold)
     roc_auc = 0  
     f1_score_value = 0
     
-    # Set the parameters by cross-validation
-#    tuned_parameters = [{'max_depth': [500, 1000, 5000], 'max_leaf_nodes': [500, 1000, 5000]}]
-    
     for train, test in skf:
-#        clf = BaggingClassifier(tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500), max_samples=0.5, max_features=0.5)    
-        clf = tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500)    
-#        clf = svm.SVC()
-#        clf = AdaBoostClassifier(tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500),
-#                         algorithm="SAMME",
-#                         n_estimators=200)
-        clf = clf.fit(traning_data.iloc[train], training_label.iloc[train])
+        classfier = classfier.fit(traning_data.iloc[train], training_label.iloc[train])
 
         #compute auc
-        probas_  = clf.predict_proba(traning_data.iloc[test])
+        probas_  = classfier.predict_proba(traning_data.iloc[test])
         fpr, tpr, thresholds = roc_curve(training_label.iloc[test], probas_[:, 0])
         roc_auc += auc(fpr, tpr)    
         
         #compute f1_score
-        label_pred = clf.predict(traning_data.iloc[test])
-        print label_pred.shape, training_label.iloc[test].shape
-        print label_pred
-        print training_label.iloc[test].values
-
-                
+        label_pred = classfier.predict(traning_data.iloc[test])
+        
         f1_score_value += f1_score(training_label.iloc[test], label_pred, pos_label= 1)
         
-    return roc_auc / fold, f1_score_value / fold
+    return roc_auc / fold, f1_score_value / fold    
+
+#testing_label and test_data isn't used temperorily
+def classify(traning_data, training_label, test_data, testing_label):
+    
+#        clf = BaggingClassifier(tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500), max_samples=0.5, max_features=0.5)    
+#        clf = tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500, class_weight={1:12}, criterion='entropy')    
+#        clf = tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500, class_weight={1:12})        
+#        clf = tree.DecisionTreeClassifier(max_depth=5)        
+#        clf = AdaBoostClassifier(tree.DecisionTreeClassifier(max_depth=5),
+#                         algorithm="SAMME",
+#                         n_estimators=100)
+#        clf = LogisticRegression()
+#        clf = KNeighborsClassifier()
+
+    print "decision tree :\n auc: %f f1_score: %f" % estimate(tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500, class_weight={1:12}),traning_data, training_label, test_data, testing_label)    
+    print "random forest:\n auc: %f f1_score: %f" % estimate(RandomForestClassifier(max_depth=500, n_estimators=10, max_leaf_nodes= 500),traning_data, training_label, test_data, testing_label)    
+    print "GradientBoostingClassifier() :\n auc: %f f1_score: %f" % estimate(LogisticRegression(),traning_data, training_label, test_data, testing_label)    
+    print "LogisticRegression() :\n auc: %f f1_score: %f" % estimate(LogisticRegression(),traning_data, training_label, test_data, testing_label)    
+    
+#    print "KNeighborsClassifier :\n auc: %f f1_score: %f" % estimate(tree.DecisionTreeClassifier(max_depth=500, max_leaf_nodes= 500, class_weight={1:12}),traning_data, training_label, test_data, testing_label)    
+
+def binning(data):
+    data = data.values
+    bins = np.linspace(0, 1, 10)
+    digitized = np.digitize(data, bins)
+    bin_means = [data[digitized == i].mean() for i in range(1, len(bins))]
 
 def selectModel(traning_data, training_label, test_data, testing_label):
-    fold = 2
-    skf = StratifiedKFold(training_label, fold)
-    roc_auc = 0  
-    f1_score_value = 0
-    
     # Set the parameters by cross-validation
-    tuned_parameters = [{'max_depth': [500, 1000, 5000], 'max_leaf_nodes': [500, 1000, 5000]}]
+    tuned_parameters = [{'max_depth': [100, 500, 1000], 'max_leaf_nodes': [100, 500, 1000], 'class_weight' : [{1:20}, {1:12}, {1:5}, {1: 1}]}]
 
-    scores = ['precision', 'recall']
+    scores = ['f1']
 
     for score in scores:
         clf = GridSearchCV(tree.DecisionTreeClassifier(), tuned_parameters, cv=5, scoring='%s_weighted' % score)    
@@ -100,16 +114,16 @@ def balance(data, label, ratio):
     data['label'] = label
     
     negative_data = data[data['label'] == -1]
-    positive_data = data[data['label'] == 1]    
+    positive_data = data[data['label'] == 1]   
     
+    print negative_data.shape
+    print positive_data.shape
+
     negative_num = int(ratio * (label[label == -1]).shape[0])
-    print negative_num
+
+    random_drop = random.sample(range(label[label == -1].shape[0]), negative_num)
+    negative_data = negative_data.reset_index(drop=True).drop(random_drop)
     
-        
-    for i in range(negative_num):
-        ran = random.randint(0, negative_num - i)
-        negative_data = negative_data.drop(negative_data.index[ran])
-            
     data = negative_data.append(positive_data) 
     label = data['label']
     
@@ -118,11 +132,15 @@ def balance(data, label, ratio):
     return data, label
     
 def selectFeature(data, label):
-    clf = ExtraTreesClassifier()
-    clf = clf.fit(data, label)
+#    clf = ExtraTreesClassifier()
+#    clf = clf.fit(data, label)
+#    clf = GradientBoostingClassifier(learning_rate=0.02, n_estimators=100, max_leaf_nodes=6)
     
-    model = SelectFromModel(clf, prefit=True)    
-    return model.transform(data)    
+    model = SelectFromModel(clf)    
+    model.fit(data, label)
+    
+#    clf = SelectPercentile(f_classif, 50)
+    return model.transform(data) 
 
 #e.g. columns = ['Var1', 'Var2']
 def labelEncoder(dataframe, columns):
@@ -133,8 +151,11 @@ def labelEncoder2(dataframe):
     
 def main():
     #read data from files
-#    training_data, testing_data = loadData('orange_small_train.data')
-    training_data, testing_data = loadData('step_preprocess.data')
+#    training_data, testing_data = loadData('step_preprocess.data')
+    print 'start to load and preprocess data'
+    training_data, testing_data = loadData('orange_small_train.data')
+    
+#    training_data, testing_data = loadData('new.data')
     appe_label, appe_label_test = loadLabel('orange_small_train_appetency.labels')
     churn_label, churn_label_test = loadLabel('orange_small_train_churn.labels')
     upsel_label, upsel_label_test = loadLabel('orange_small_train_upselling.labels')
@@ -143,33 +164,35 @@ def main():
 #    training_data = replaceMissingValue(training_data)
 #    testing_data = replaceMissingValue(testing_data)        
     
-    #retrieve parts of data
-#    part_testing_data = pd.DataFrame(selectFeature(testing_data, appe_label_test))
-#    part_training_data = pd.DataFrame(selectFeature(training_data, appe_label))
-    part_testing_data = testing_data
-    part_training_data = training_data
+#    print 'feature selection'
+    part_testing_data = pd.DataFrame(selectFeature(testing_data, appe_label_test))
+    part_training_data = pd.DataFrame(selectFeature(training_data, appe_label))
+#    part_testing_data = testing_data
+#    part_training_data = training_data
+
+    print 'balance data'    
     
-#    part_training_data, appe_label = balance(part_training_data, appe_label, 0.3)
+#    part_training_data_appe, appe_label = balance(part_training_data, appe_label, 0.)
+    part_training_data_churn, churn_label = balance(part_training_data, churn_label, 0.0)
+#    part_training_data_upsel, upsel_label = balance(part_training_data, upsel_label, 0.1)    
     
-    print part_training_data.shape, appe_label.shape
-#    part_testing_data = testing_data.iloc[:, 100:103]
-#    part_training_data = training_data.iloc[:, 10:103]
+#    part_testing_data = testing_data.iloc[:, 0:50]
+#    part_training_data = training_data.iloc[:, 0:50]
 #    part_testing_data = balance(part_training_data, appe_label, 0.3)        
-#    part_training_data = balance(part_training_data, appe_label, 0.3)
-    
-    #transform caterical data into integer  
-#    part_testing_data = labelEncoder(part_testing_data, ['Var221', 'Var222', 'Var223'])
-#    part_training_data = labelEncoder(part_training_data, ['Var221', 'Var222', 'Var223'])
-#    testing_data = labelEncoder2(testing_data)
-#    training_data = labelEncoder2(training_data)     
+#    part_training_data = balance(part_training_data, appe_label, 0.3)   
 
-
+#    print 'start to select model'
 #    selectModel(part_training_data, appe_label, part_testing_data, appe_label_test)    
      
 #    print part_testing_data
-    print "appe: %f f1_score: %f" % classify(part_training_data, appe_label, part_testing_data, appe_label_test)    
-#    print "churn: %f" % classify(part_training_data, churn_label, part_testing_data, churn_label_test)    
-#    print "upsel: %f" % classify(part_training_data, upsel_label, part_testing_data, upsel_label_test)        
+    print 'star to classify'
+    
+    print 'churn:'
+    classify(part_training_data_churn, churn_label, part_testing_data, churn_label_test)    
+    
+    print "\nappe:"  
+#    classify(training_data, appe_label, testing_data, appe_label_test)    
+#    print "upsel: %f f1_score: %f" % classify(part_training_data_upsel, upsel_label, part_testing_data, upsel_label_test)        
 #    print 'star to classify'
 #    print "appe: %f" % classify(training_data, appe_label, testing_data, appe_label_test)    
 #    print "churn: %f" % classify(training_data, churn_label, testing_data, churn_label_test)    
---
+++ split.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 20:34:56 2016
+
+@author: asus1
+"""
+import numpy as np
+import pandas as pd
+import MutiLabelEncoder as labelencoder
+
+def labelEncoder(dataframe, columns):
+    return labelencoder.MultiColumnLabelEncoder(columns=columns).transform(dataframe)
+
+def getTwoTypesData(filename):
+    raw = pd.read_csv(filename, sep='\t')
+    data = raw.dropna(axis=0, how='all')
+    data = raw.dropna(axis=1, how='all')
+    
+    data.fillna(data.mean(), inplace=True)   
+    
+    for i in range(data.columns.shape[0]):
+        col_name = data.columns.values[i]
+        data[col_name].fillna(data[col_name].mode()[0], inplace=True)
+    
+    newData = data
+    for i in range(data.columns.shape[0]):
+        col_name = data.columns.values[i]
+        temp = data.loc[0, col_name]
+        flag = 0    
+        for x in data[col_name]:
+            if x != temp:
+                flag = 1
+        if flag == 0:
+            newData = newData.drop(col_name, axis=1)
+    data = newData
+    
+    #print data
+    
+    columns = []
+    columns_discrete = []
+    columns_numeric = []
+    for i in range(data.columns.shape[0]):
+        col_name = data.columns.values[i]
+        columns.append(col_name)
+        if data[col_name].dtypes == np.object:
+            columns_discrete.append(col_name)
+        else:
+            columns_numeric.append(col_name)
+            
+    #print columns_discrete,
+    #print columns_numeric
+    
+    data = labelEncoder(data, columns_discrete)
+    
+    for col in columns_numeric:
+        arr = pd.cut(data[col], 10).astype('object')
+        data[col] = arr
+    
+    data = labelEncoder(data, columns)    
+    
+#    return data[columns_discrete], data[columns_numeric]
+    return data
